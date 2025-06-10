ba0deb3240f1038c3e36cd4e9a4ec31f412822b1
MonsieurC <mail.anthony42@gmail.com>
2025-06-10 14:58:22 +0200

    some fix for mariadb script

--- srcs/requirements/mariadb/conf/mariadb.sh
+++ srcs/requirements/mariadb/conf/mariadb.sh
@@ -1,32 +1,42 @@
 #!/bin/sh
 
+
+# DB_ALREADY_EXISTS=$(mysql -uroot -p"$SQL_ROOT_PASSWORD" -e "SHOW DATABASES" | grep "$SQL_DATABASE" | wc -l);
+
 /etc/init.d/mariadb start;
 
-DB_ALREADY_EXISTS=$(mysql -uroot -p$SQL_ROOT_PASSWORD -e "SHOW DATABASES" | grep $SQL_DATABASE | wc -l);
-
-if [ $DB_ALREADY_EXISTS -eq 1 ]; then
-	echo "Database already exists";
-else
+# if [ "$DB_ALREADY_EXISTS" -eq 1 ]; then
+# 	echo "Database already exists";
+# else
 	echo "Database does not exist";
 	mysql_secure_installation << END
-Y
-$SQL_ROOT_PASSWORD
-$SQL_ROOT_PASSWORD
-Y
-Y
-Y
-Y
+	
+	n
+	Y
+	Y
+	Y
+	Y
 END
+wait $!
 
+	echo "Creating database and user";
+	mysql -uroot -p"$SQL_ROOT_PASSWORD" -e "ALTER USER 'root' @'localhost' IDENTIFIED BY '$SQL_ROOT_PASSWORD';"
 
-mysql -uroot -p$SQL_ROOT_PASSWORD -e "CREATE DATABASE IF NOT EXISTS $SQL_DATABASE;"
+echo "Creating database: $SQL_DATABASE";
+	mysql -uroot -p"$SQL_ROOT_PASSWORD" -e "CREATE DATABASE IF NOT EXISTS \`$SQL_DATABASE\`;"
+	
+echo "Creating user: $SQL_USER";
+	mysql -uroot -p"$SQL_ROOT_PASSWORD" -e "CREATE USER '$SQL_USER'@'%' IDENTIFIED BY '$SQL_PASSWORD'; GRANT ALL PRIVILEGES ON \`$SQL_DATABASE\`.* TO '$SQL_USER'@'%'; FLUSH PRIVILEGES;"
 
-mysql -uroot -p$SQL_ROOT_PASSWORD -e "CREATE USER '$SQL_USER'@'%' IDENTIFIED BY '$SQL_PASSWORD'; GRANT ALL PRIVILEGES ON $SQL_DATABASE.* TO '$SQL_USER'@'%'; FLUSH PRIVILEGES;";
+echo "Database and user created successfully";
 
-fi
+# fi
+
 
 sleep 1;
 
-/etc/init.d/mariadb stop;
+#/etc/init.d/mariadb stop;
+mariadb-admin -u root --password="$SQL_ROOT_PASSWORD" shutdown;
 
-exec "$@";
+# exec "$@";
+exec mariadbd-safe
